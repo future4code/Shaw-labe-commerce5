956d3e6c39ffb2a3099fe29edef3d9fa772f4564
Regiscleia Dias Amorim Silva <regiscleiaa@gmail.com>
2022-03-31 12:09:20 -0300

    alteraçoes

--- src/App.js
+++ src/App.js
@@ -5,47 +5,18 @@ import botas from "./img/botas.webp";
 import capacete from "./img/capacete.jpg";
 import camiseta from "./img/camiseta.jpg";
 import Home from "./components/Home"
+import productList from "./data/products.json"
 
 class App extends React.Component {
   state = {
-    produtos: [{
-      id: 1,
-      nome: "Botas de astronautas",
-      valor: "150,00",
-      imagem: 'botas.webp'
-    },
-    {
-      id: 2,
-      nome: "Capacete de astronauta",
-      valor: "350,99",
-      imagem: 'capacete.jpg'
-    },
-    {
-      id: 3,
-      nome: "Camiseta astronautas",
-      valor: "109,90",
-      imagem: 'camiseta.jpg'
-    }
-    ]
+    produtos: productList
 
   };
 
   render() {
     return (
-      
-
-      <div className='container'>
-        <div className='filtro'>
-          
-        </div>
-        <div className='home'>
-          <Home produtos = {this.state.produtos} >
-          </Home>
-        </div>
-        <div className='carrinho'>
-          
-        </div>
-      </div>
+      <Home produtos = {this.state.produtos} >
+      </Home>
     )
   }
 }
